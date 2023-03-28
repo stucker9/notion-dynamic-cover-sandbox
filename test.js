2c33fc7ae72eac1cb402fb51ba8e855062f9c19f
stucker9 <dz9c92@gmail.com>
2023-03-28 15:42:48 -0400

    test5

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 // Retrieve weather data from API
 const apiKey = 'a6f65b75c0f7529f4fc1321f2bf0c794';
 const location = 'Providence,US';
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=imperial`;
 
 fetch(apiUrl)
   .then(response => response.json())
@@ -13,8 +13,8 @@ fetch(apiUrl)
     // Generate SVG for weather widget
     const svg = SVG('svg-container').size(500, 200);
     svg.rect(500, 200).fill('#FFF');
-    svg.text(`Current temperature: ${temp}°C`).move(20, 50);
-    svg.text(`Feels like: ${feels_like}°C`).move(20, 80);
+    svg.text(`Current temperature: ${temp}°F`).move(20, 50);
+    svg.text(`Feels like: ${feels_like}°F`).move(20, 80);
     svg.text(`Humidity: ${humidity}%`).move(20, 110);
     svg.text(description).move(20, 140);
     svg.image(`https://openweathermap.org/img/wn/${icon}.png`, 100, 140, 50, 50);
